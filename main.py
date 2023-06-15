1154bdd8d2b3f2d863bd5fb66492bf08b68d9bfa
heyhenry <henryyy3@hotmail.com>
2023-06-15 14:32:11 +0800

    intial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template, redirect, url_for, request, session
+import requests
+import json
+from datetime import datetime
+
+app = Flask(__name__)
+# app.secret_key = ''
+
+@app.route('/', methods=['GET', 'POST'])
+def login():
+    
+
+if __name__ == "__main__":
+    app.run()
