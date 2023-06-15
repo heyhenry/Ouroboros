8804fe4edc434362f9483b735fe0cc845f42f0f5
heyhenry <henryyy3@hotmail.com>
2023-06-15 14:50:17 +0800

    First deployable instance.

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def login():
+    return 'Hello World!'
     
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run()
