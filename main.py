c5ff0a7ffed2ade8cbfa237a9fb097744be6cfcd
heyhenry <henryyy3@hotmail.com>
2023-06-15 15:11:31 +0800

    Created simple login with validation. Simple forum page created.

--- main.py
+++ main.py
@@ -6,9 +6,42 @@ from datetime import datetime
 app = Flask(__name__)
 # app.secret_key = ''
 
+# functions
+
+def check_login_credentials(username, password):
+
+    # temporary roleplaying db stored credentials:
+    db_username = 'yaka' 
+    db_password = 'waka'
+
+    if username == db_username and password == db_password:
+        return True
+    else:
+        return False
+
 @app.route('/', methods=['GET', 'POST'])
 def login():
-    return 'Hello World!'
     
+    error = None
+
+    if request.method == 'POST':
+
+        username = request.form['username']
+        password = request.form['password']
+
+        if check_login_credentials(username, password):
+
+            return redirect(url_for('forum'))
+
+        else:
+            error = 'Unsuccessful login. Please try again.'
+
+    return render_template('login.html', error=error)
+    
+@app.route('/forum', methods=['GET', 'POST'])
+def forum():
+
+    return render_template('forum.html')
+
 if __name__ == '__main__':
     app.run()
